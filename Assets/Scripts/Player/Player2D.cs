8af1de72f9094bcec52f01af892b051a43c3856c
Sunil Rao <slysunil101@gmail.com>
2016-07-10 15:27:14 -0700

    Added basic 2D movement

---
+++ Assets/Scripts/Player/Player2D.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player2D : MonoBehaviour 
+{
+	// Player component instances
+	private Rigidbody o_rigidbody;
+
+	// Movement
+	public float movementSpeed;
+	public float maxSpeed;
+
+	void Start () 
+	{
+		// Instantiate component instances
+		o_rigidbody = GetComponent<Rigidbody>();
+
+
+	}
+	
+	void Update () 
+	{
+		input();
+	}
+
+	private void input()
+	{
+		playerControls();
+	}
+
+	private void playerControls()
+	{
+		Vector2 movementDirection = new Vector2();
+
+		if (o_rigidbody.velocity.x < maxSpeed)
+		{
+			movementDirection.x = Input.GetAxisRaw("Horizontal") * movementSpeed;
+		}
+		if (o_rigidbody.velocity.y < maxSpeed)
+		{
+			movementDirection.y = Input.GetAxisRaw("Vertical") * movementSpeed;
+		}
+
+		o_rigidbody.AddForce(movementDirection);
+	}
+
+	void OnCollisionEnter(Collision other)
+	{
+
+	}
+
+	void OnCollisionExit(Collision other)
+	{
+		
+	}
+}
