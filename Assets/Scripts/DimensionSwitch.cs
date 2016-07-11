484e9eca844a9b8cd21f5ebc7a615ed3061b78a7
Sunil Rao <slysunil101@gmail.com>
2016-07-10 17:55:10 -0700

    Added dimension switching

---
+++ Assets/Scripts/DimensionSwitch.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DimensionSwitch : MonoBehaviour 
+{
+	public GameObject player2DPrefab;
+	public GameObject player3DPrefab;
+	private bool is3D = false;
+	public bool canSwitch = true;
+
+	void Start () 
+	{
+	
+	}
+	
+	void Update () 
+	{
+	
+	}
+
+	void dimensionSwitch(Vector3 spawnPosition)
+	{
+		spawnPosition.z = 0;
+
+		if (is3D)
+		{
+
+			GameObject player2D = (GameObject) Instantiate(player2DPrefab, spawnPosition, Quaternion.identity);
+
+
+			is3D = false;
+		}
+		else
+		{
+			GameObject player3D = (GameObject) Instantiate(player3DPrefab, spawnPosition, Quaternion.identity);
+			
+			is3D = true;
+		}
+	}
+
+	public void initiate(GameObject inputObject)
+	{
+		if (canSwitch)
+		{
+			Destroy (inputObject);
+			dimensionSwitch(inputObject.transform.position);
+			//other.gameObject.GetComponent<Rigidbody>().AddForce(Vector3.right * 5);
+			StartCoroutine(dimensionSwitchCooldown());
+		}
+	}
+
+	void OnCollisionEnter(Collision other)
+	{
+		if (other.gameObject.CompareTag("Player"))
+		{
+			Debug.Log("Switching dimensions");
+			initiate(other.gameObject);
+		}
+	}
+
+	IEnumerator dimensionSwitchCooldown()
+	{
+		canSwitch = false;
+		yield return new WaitForSeconds(1);
+		canSwitch = true;
+	}
+}
---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotate : MonoBehaviour 
+{
+	public float rotationSpeed;
+
+	public bool xRotation;
+	public bool yRotation;
+	public bool zRotation;
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (xRotation)
+		{
+			transform.Rotate(Vector3.right * rotationSpeed);
+		}
+		if (yRotation)
+		{
+			transform.Rotate(Vector3.up * rotationSpeed);
+		}
+		if (zRotation)
+		{
+			transform.Rotate(-Vector3.forward * rotationSpeed);
+		}
+	}
+}
