0f437b38d3b86e1f4e371e00be2d9db9d9db7b25
Sunil Rao <slysunil101@gmail.com>
2016-07-16 10:09:20 -0700

    Finished cyclops

---
+++ Assets/Scripts/2D AI/Cyclops.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cyclops : MonoBehaviour 
+{
+	private Rigidbody c_rigidbody;
+	public float switchInterval;
+	private bool isMoving;
+	private bool isRight = true;
+	public float movementForce;
+
+	void Start () 
+	{
+		c_rigidbody = GetComponent<Rigidbody>();
+
+		isMoving = true;
+
+		StartCoroutine(cyclopsSwitchDirection());
+		StartCoroutine(cyclopsMovement());
+	}
+
+	void Update () 
+	{
+	
+	}
+
+	IEnumerator cyclopsMovement()
+	{
+		while (isMoving)
+		{
+			yield return new WaitForSeconds(0.05f);
+
+			if (isRight)
+			{
+				c_rigidbody.velocity = (Vector3.right * movementForce);
+			}
+			else
+			{
+				c_rigidbody.velocity = (Vector3.left * movementForce);
+			}
+		}
+	}
+
+	IEnumerator cyclopsSwitchDirection()
+	{
+		while (isMoving)
+		{
+			yield return new WaitForSeconds(switchInterval);
+
+			if (isRight)
+			{
+				isRight = false;
+			}
+			else
+			{
+				isRight = true;
+			}
+
+			Vector3 newScale = transform.localScale;
+			newScale.x *= -1;
+			transform.localScale = newScale;
+		}
+	}
+}
--- Assets/Scripts/2D AI/Radish.cs
+++ Assets/Scripts/2D AI/Radish.cs
@@ -37,7 +37,7 @@ public class Radish : MonoBehaviour
 	{
 		while (isMoving)
 		{
-			yield return new WaitForSeconds(0.05f);
+			yield return new WaitForSeconds(0.1f);
 
 			if (currentDirection == Direction.right)
 			{
