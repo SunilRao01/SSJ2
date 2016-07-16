42c7be1a167566c6f9eb52fd696271fde7ea5f49
Sunil Rao <slysunil101@gmail.com>
2016-07-16 09:42:02 -0700

    2/3 2D NPCs implemented

---
+++ Assets/Scripts/2D AI/Mumble.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mumble : MonoBehaviour 
+{
+	private Rigidbody c_rigidbody;
+
+	public float movementInterval;
+	private bool isCycling;
+	private bool isRight = true;
+	private int cycleIndex = 1;
+
+	public float movementForce;
+
+	void Start () 
+	{
+		c_rigidbody = GetComponent<Rigidbody>();
+
+		isCycling = true;
+		StartCoroutine(mumbleMovement());
+	}
+	
+
+	void Update () 
+	{
+	
+	}
+
+	IEnumerator mumbleMovement()
+	{
+		while (isCycling)
+		{
+			yield return new WaitForSeconds(movementInterval);
+
+			Vector3 movementVector = new Vector3(movementForce/3, movementForce, 0);
+
+			if (cycleIndex < 3)
+			{
+				if (isRight)
+				{
+					cycleIndex++;
+				}
+				else
+				{
+					if (cycleIndex != 1)
+					{
+						cycleIndex--;
+						movementVector.x *= -1;
+					}
+					else
+					{
+						isRight = true;
+						cycleIndex++;
+					}
+				}
+			}
+			else
+			{
+				if (isRight)
+				{
+					isRight = false;
+				}
+				else
+				{
+					isRight = true;
+				}
+					
+				movementVector.x *= -1;
+				cycleIndex--;
+			}
+
+			c_rigidbody.AddForce(movementVector);
+		}
+	}
+}
---
+++ Assets/Scripts/2D AI/Radish.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Radish : MonoBehaviour 
+{
+	enum Direction
+	{
+		left,
+		right,
+		up,
+		down
+	}
+
+	// Components
+	private Rigidbody c_rigidbody;
+
+	public float movementForce;
+	private bool isMoving;
+	private Direction currentDirection;
+
+	void Awake () 
+	{
+		c_rigidbody = GetComponent<Rigidbody>();
+
+		currentDirection = Direction.right;
+
+		isMoving = true;
+		StartCoroutine(radishMovement());
+	}
+	
+	void Update () 
+	{
+	
+	}
+
+	IEnumerator radishMovement()
+	{
+		while (isMoving)
+		{
+			yield return new WaitForSeconds(0.05f);
+
+			if (currentDirection == Direction.right)
+			{
+				c_rigidbody.velocity = (Vector3.right * movementForce);
+			}
+			else if (currentDirection == Direction.left)
+			{
+				c_rigidbody.velocity = (Vector3.left * movementForce);
+			}
+		}
+	}
+
+	void OnCollisionEnter(Collision other)
+	{
+		if (other.gameObject.CompareTag("DirectionSwitch"))
+		{
+			if (currentDirection == Direction.right)
+			{
+				currentDirection = Direction.left;
+
+				Vector3 newScale = transform.localScale;
+				newScale.x *= -1;
+				transform.localScale = newScale;
+			}
+			else
+			{
+				currentDirection = Direction.right;
+				
+				Vector3 newScale = transform.localScale;
+				newScale.x *= -1;
+				transform.localScale = newScale;
+			}
+		}
+	}
+}
